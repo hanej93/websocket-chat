505adc5580a90fbb129c19a5013893e93cc05fab
EJ <hanej93@naver.com>
2023-02-26 03:02:02 +0900

    Stomp 를 이용한 채팅 서비스

---
+++ src/main/java/com/example/chatting/conf/EmbeddedRedisConfig.java
@@ -0,0 +1,4 @@
+package com.example.chatting.conf;
+
+public class EmbeddedRedisConfig {
+}
---
+++ src/main/java/com/example/chatting/dto/ChatRoom.java
@@ -0,0 +1,19 @@
+package com.example.chatting.dto;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ChatRoom {
+    private String roomId;
+    private String name;
+
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
+    }
+}
