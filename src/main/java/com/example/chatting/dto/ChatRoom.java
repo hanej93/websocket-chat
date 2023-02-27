e05f0cfb425c1618adde8d2dd11774426eed312d
EJ <hanej93@naver.com>
2023-02-27 20:39:50 +0900

    Redis pub/sub  구현

--- src/main/java/com/example/chatting/dto/ChatRoom.java
+++ src/main/java/com/example/chatting/dto/ChatRoom.java
@@ -1,12 +1,17 @@
 package com.example.chatting.dto;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
+
     private String roomId;
     private String name;
 
