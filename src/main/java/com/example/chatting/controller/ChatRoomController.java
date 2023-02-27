e55176a075426b0f0ebcb95eb6f28a0b92a973a9
EJ <hanej93@naver.com>
2023-02-27 22:14:08 +0900

    Spring Security + JWT 보안 적용

--- src/main/java/com/example/chatting/controller/ChatRoomController.java
+++ src/main/java/com/example/chatting/controller/ChatRoomController.java
@@ -1,8 +1,12 @@
 package com.example.chatting.controller;
 
 import com.example.chatting.dto.ChatRoom;
+import com.example.chatting.dto.LoginInfo;
 import com.example.chatting.repository.ChatRoomRepository;
+import com.example.chatting.service.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +19,15 @@ import java.util.List;
 public class ChatRoomController {
 
     private final ChatRoomRepository chatRoomRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @GetMapping("/user")
+    @ResponseBody
+    public LoginInfo getUserInfo() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        return LoginInfo.builder().name(name).token(jwtTokenProvider.generateToken(name)).build();
+    }
 
     // 채팅 리스트 화면
     @GetMapping("/room")
